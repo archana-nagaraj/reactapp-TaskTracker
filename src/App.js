b78e6a133e15c8692d2d6e279d7d25ce570009a4
Archana Nagaraja <johndoe@example.com>
2021-09-01 11:46:38 -0700

    added tasks and task component, installed react-icons dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+//  hook called useState from react directory
+import { useState } from "react"
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
-
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'Shopping - pickup',
+        day: 'Sep 1st at 4:00PM',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Shopping - drop',
+        day: 'Sep 2st at 5:00PM',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Shopping ',
+        day: 'Sep 3st at 1:00PM',
+        reminder: false,
+    },
+])
   return (
     <div className = "container">
       <Header />
+      <Tasks tasks ={tasks}/>
+
     </div>
-  );
+  )
 }
 
 export default App;
